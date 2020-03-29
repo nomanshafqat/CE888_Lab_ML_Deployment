21e7acfebff266fd316a1321bd62f50a1b80d348
Noman Shafqat <14besemshafqat@seecs.edu.pk>
2020-03-29 20:03:26 +0100

    final push

--- app.py
+++ app.py
@@ -5,32 +5,19 @@ import pickle
 print(os.getcwd())
 path = os.getcwd()
 
-with open('Models/logistic_model.pkl', 'rb') as f:
-    logistic = pickle.load(f)
-
-with open('Models/RF_model.pkl', 'rb') as f:
-    randomforest = pickle.load(f)
-
-with open('Models/svm_clf_model.pkl', 'rb') as f:
-    svm_model = pickle.load(f)
 
 
-def get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model):
-    mylist = [Driver_Age, Tax, price, Licence_Length_Years]
-    mylist = [float(i) for i in mylist]
-    vals = [mylist]
+with open('Models/mlp.pkl', 'rb') as f:
+    mlp = pickle.load(f)
 
-    if req_model == 'Logistic':
+
+def get_predictions(feature_map_array, req_model):
+
+    vals = [feature_map_array]
+    if req_model == 'MLP':
         #print(req_model)
-        return logistic.predict(vals)[0]
+        return mlp.predict(vals)[0]
 
-    elif req_model == 'RandomForest':
-        #print(req_model)
-        return randomforest.predict(vals)[0]
-
-    elif req_model == 'SVM':
-        #print(req_model)
-        return svm_model.predict(vals)[0]
     else:
         return "Cannot Predict"
 
@@ -43,18 +30,35 @@ def my_form():
 
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
-    price = request.form['price']
-    Tax = request.form['Tax']
-    Driver_Age = request.form['Driver_Age']
-    Licence_Length_Years = request.form['Licence_Length_Years']
+
+
+
+    age = request.form['age']
+    sex = request.form['sex']
+    cp = request.form['cp']
+    trestbps = request.form['trestbps']
+
+    chol = request.form['chol']
+    fbs = request.form['fbs']
+    restecg = request.form['restecg']
+    thalach = request.form['thalach']
+    exang = request.form['exang']
+
+    oldpeak = request.form['oldpeak']
+    slopethe = request.form['slope']
+    ca = request.form['ca']
+    thal = request.form['thal']
     req_model = request.form['req_model']
 
-    target = get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model)
+    mylist = [age,sex,cp,trestbps,chol, fbs, restecg, thalach,exang,oldpeak,slopethe,ca,thal]
+    mylist = [float(i) for i in mylist]
+
+    target = get_predictions(mylist, req_model)
 
     if target==1:
-        sale_making = 'Customer is likely to buy the insurance'
+        sale_making = 'Patient is likely to have heart disease'
     else:
-        sale_making = 'Customer is unlikely to buy the insurance'
+        sale_making = 'Patient is unlikely to have heart disease'
 
     return render_template('home.html', target = target, sale_making = sale_making)
 
